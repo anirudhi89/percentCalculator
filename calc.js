a75fad33ea9a961d55c718042688a20f98f6f5f4
Anirudh Iyer <anirudh.iyer@outlook.com>
2021-07-24 17:16:33 -0700

    finally working

--- calc.js
+++ calc.js
@@ -1,1 +1,52 @@
-var x = 0;
+var numField1 = document.getElementById("numField1")
+var numField2 = document.getElementById("numField2")
+var result = document.getElementById("result")
+
+var form = document.getElementById("xisWhatPercentofY")
+
+var printRes = document.getElementById('result')
+
+function isNumeric(n) {
+    return !isNaN(parseFloat(n)) && isFinite(n)
+}
+
+
+function myCalFunct() {
+
+    printRes.innerText = "";
+
+
+    if (!numField1.value || !numField2.value) {
+        alert("Please enter values for both fields")
+    }
+    else if (isNumeric(numField1.value) == false || isNumeric(numField2.value) == false) {
+        alert("Make sure they are both numbers")
+    }
+    else {
+        // alert("math") Working
+        percentCalc()
+
+    }
+    
+    function percentCalc() {
+        let x = numField1.value;
+        let y = numField2.value;
+        var mid = (y/x) * 1.0
+        var ans = 100.0/mid
+        //alert("Your answer is " + ans + "%")
+        printRes.innerText = ("Result " + ans + "%")
+    }
+    
+    
+}
+
+
+form.addEventListener('submit', myCalFunct)
+
+
+
+
+
+
+
+
